9c9493fc9408fb6801822505ea3815694e7d2e3b
RipcordSoftware <craig@ripcordsoftware.com>
2015-01-02 17:51:07 +0000

    Added missing DotNetZipLib files and stub Example project

---
+++ HttpWebServerExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HttpWebServerExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
