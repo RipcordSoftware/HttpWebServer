291ea30a1b0c4a004401a5820dea5bc245d964ab
Craig Minihan <craig@ripcordsoftware.com>
2019-02-13 23:40:30 +0000

    Added simple BufferManager unit tests

---
+++ HttpWebServer.UnitTests/TestHttpWebBufferManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+using RipcordSoftware.HttpWebServer;
+
+namespace RipcordSoftware.HttpWebServer.UnitTests
+{
+    [TestFixture()]
+    public class TestHttpWebBufferManager
+    {
+        private Interfaces.IHttpWebBufferManager _bufferManager;
+
+        [SetUp()]
+        public void SetUp()
+        {
+            _bufferManager = new HttpWebBufferManager();
+        }
+
+        [Test()]
+        public void TestGetBuffer()
+        {
+            var buffer = _bufferManager.GetBuffer(1024);
+
+            Assert.IsNotNull(buffer);
+            Assert.AreEqual(1024, buffer.Length);
+        }
+
+        [Test()]
+        public void TestGetBufferDuplicate()
+        {
+            var buffer1 = _bufferManager.GetBuffer(1024);
+            var buffer2 = _bufferManager.GetBuffer(1024);
+
+            Assert.IsNotNull(buffer1);
+            Assert.AreEqual(1024, buffer1.Length);
+
+            Assert.IsNotNull(buffer2);
+            Assert.AreEqual(1024, buffer2.Length);
+
+            Assert.AreNotSame(buffer1, buffer2);
+        }
+    }
+}
